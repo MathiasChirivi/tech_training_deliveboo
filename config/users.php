a281a0ed7f42cde59a6d9d237a1a24ae087d7676
Lip-7 <zhighofficial@gmail.com>
2023-11-29 11:35:43 +0100

    init user cofig

---
+++ config/users.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    [
+        'name' => 'Giuliano',
+        'lastname' => 'Gostinfini',
+        'email' => 'user1@gmail.com',
+        'rest_name' => 'Pizzeria Napoletana',
+        'rest_address' => 'Via Roma 36, Torino',
+        'rest_piva' => '1545262584452',
+        'rest_photo' => 'https://media-assets.lacucinaitaliana.it/photos/61fac9aebe6f37f54cf8d0de/16:9/w_2560%2Cc_limit/pizzeria-popolare.jpg',
+        'typologies' => 'pizza',
+        'dishes' => [
+            [
+                'name' => 'Pizza Margherita',
+            ]
+        ]
+    ]
+];
